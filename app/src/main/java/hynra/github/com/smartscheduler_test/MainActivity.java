661a00636b930a24331747ca7fca4d0fb35660f9
hynra <hynra@yahoo.com>
2017-09-12 14:41:01 +0700

    init project

---
+++ app/src/main/java/hynra/github/com/smartscheduler_test/MainActivity.java
@@ -0,0 +1,13 @@
+package hynra.github.com.smartscheduler_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
